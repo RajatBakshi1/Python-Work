b95ffa8759727b065daaeaa1cb48de43cadd6671
RajatBakshi1 <rajat.bakshi03@gmail.com>
2018-08-08 15:59:43 +0530

    Python

---
+++ assign.py
@@ -0,0 +1,37 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> # Q1:- Print anything you want on screen.
+>>> name="Rajat Bakshi"
+>>> print("My name is",name)
+My name is Rajat Bakshi
+>>> # Q2:- Join two strings using '+'.
+>>> str1="Rajat"
+>>> str2="Bakshi"
+>>> str1+str2
+'RajatBakshi'
+>>> # Q3:- Take the input of three variables x, y and z.Print their values on screen.
+>>> x=10
+>>> y=20
+>>> z=30
+>>> print("Value of x:",x, "\n Value of y:",y, "\n Value of z:",z)
+Value of x: 10 
+ Value of y: 20 
+ Value of z: 30
+>>> # Q4:- Print "Let's get started" on screen.
+>>> print("\"Let\'s get started\"")
+"Let's get started"
+>>> # Q5:- Print the following values using placeholders.
+>>> name="Rajat Bakshi"
+>>> course="Python"
+>>> fees=12500
+>>> print("name=%s \n course=%s \n fees=%d"%("\"Rajat Bakshi\"","\"Python\"",12500))
+name="Rajat Bakshi" 
+ course="Python" 
+ fees=12500
+>>> # Q6:- Let's do some exercise.
+>>> name="Rajat Bakshi"
+>>> salary=100000
+>>> print("name=%s \n salary=%d"%("Rajat Bakshi", 100000))
+name=Rajat Bakshi 
+ salary=100000
+>>> 
